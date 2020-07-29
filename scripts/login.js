6c9ac077d5b218d439bec170c2732b9d0afd227e
kylenvu <54590479+kylenvu@users.noreply.github.com>
2020-07-28 22:45:46 -0700

    Add files via upload

--- scripts/login.js
+++ scripts/login.js
@@ -8,14 +8,15 @@ btn.onclick = function() {
 function onSignIn(googleUser) {
   // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-
+  // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  // console.log('Full Name: ' + profile.getName());
+  // console.log('Given Name: ' + profile.getGivenName());
+  // console.log('Family Name: ' + profile.getFamilyName());
+  // console.log("Image URL: " + profile.getImageUrl());
+  // console.log("Email: " + profile.getEmail());
+  alert("Welcome to Beer Logger " + profile.getName());
   window.sessionStorage.setItem("profile_image_url", profile.getImageUrl());
+  document.location.reload();
 
   // The ID token you need to pass to your backend:
   var id_token = googleUser.getAuthResponse().id_token;
